d7789db569036cbaaa8ff618fd348eb4dd943910
olya <olu>
2022-08-06 12:57:29 +0200

    edits

--- src/lesson2/Task12.java
+++ src/lesson2/Task12.java
@@ -11,18 +11,17 @@ public class Task12 {
             array[i] = rn.nextInt(10) + 1;
         }
 
-        int[] array2 = Arrays.copyOf(array, 10);;
+        int[] array2 = Arrays.copyOf(array, 10);
         boolean swapped;
         do {
             swapped = false;
             for (int i = 1; i < array2.length; i++) {
                 if ((array2[i - 1] > array2[i])) {
-                    if (i - 1 != i) {
                         int temp = array2[i - 1];
                         array2[i - 1] = array2[i];
-                        array2[i] = temp;
-                    }
+                        array2[ i] =temp;
                     swapped = true;
+
                 }
             }
         } while (swapped);
