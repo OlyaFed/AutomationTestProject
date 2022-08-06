ffeca8984ac27d76163baf212e81d1b392f5893a
olya <olu>
2022-08-06 12:38:10 +0200

    factorial with while in lesson 2, task5

--- src/lesson2/Task5.java
+++ src/lesson2/Task5.java
@@ -2,12 +2,24 @@ package lesson2;
 
 public class Task5 {
     public static void main(String[] args) {
-        int i,fact = 1;
-        int number = 8;
-        for (i=1; i <= number; i++) {
-            fact = fact * i;
+       // int i,fact = 1;
+       // int number = 8;
+       // for (i=1; i <= number; i++) {
+           // fact = fact * i;
 
-        }
-        System.out.println("Factorial of " + number + " is: " + fact);
+        //}
+       // System.out.println("Factorial of " + number + " is: " + fact);
+    //}
+    int number = 8;
+    long fact = 1;
+    int i = 1;
+        while(i<=number)
+    {
+        fact = fact * i;
+        i++;
     }
+        System.out.println("Factorial of "+number+" is: "+fact);
 }
+}
+
+
